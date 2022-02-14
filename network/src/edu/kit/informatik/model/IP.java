50b030f38216752a7d705d0e0c103341f28a9f6d
Daniel <danielvalean0@gmail.com>
2022-02-14 16:01:37 +0100

    initial classes and test

---
+++ network/src/edu/kit/informatik/model/IP.java
@@ -0,0 +1,17 @@
+package edu.kit.informatik.model;
+
+public class IP implements Comparable<IP> {
+
+    public IP(final String pointNotation) throws ParseException {
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public int compareTo(IP o) {
+        return 0;
+    }
+}
---
+++ network/src/edu/kit/informatik/model/ParseException.java
@@ -0,0 +1,7 @@
+package edu.kit.informatik.model;
+
+public class ParseException extends Exception {
+    public ParseException(String message) {
+        super(message);
+    }
+}
