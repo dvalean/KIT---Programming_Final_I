b77f978c3a5cfe5472b4dcb2e35b9f52b7cdd703
Daniel <danielvalean0@gmail.com>
2022-02-23 11:33:58 +0100

    comments for Edge and Node classes

--- network/src/edu/kit/informatik/model/network/graph/Edge.java
+++ network/src/edu/kit/informatik/model/network/graph/Edge.java
@@ -2,20 +2,42 @@ package edu.kit.informatik.model.network.graph;
 
 import edu.kit.informatik.model.IP;
 
+/**
+ * Represents an Edge of a Graph.
+ * 
+ * @author uiltc
+ * @version 1.0
+ */
 public class Edge {
-    private IP source;
-    private IP destination;
+    private final IP source;
+    private final IP destination;
 
+    /**
+     * Creates a new instance of an Edge.
+     * 
+     * @param source IP value, where the Edge begins
+     * @param destination IP value, where the Edge ends
+     */
     public Edge(IP source, IP destination) {
         this.source = source;
         this.destination = destination;
     }
 
+    /**
+     * Gets the source of the Edge.
+     * 
+     * @return IP value
+     */
     public IP getSource() {
         return this.source;
     }
 
+    /**
+     * Gets the destination of the Edge.
+     * 
+     * @return IP value
+     */
     public IP getDestination() {
-        return destination;
+        return this.destination;
     }
 }
--- network/src/edu/kit/informatik/model/network/tree/Node.java
+++ network/src/edu/kit/informatik/model/network/tree/Node.java
@@ -5,46 +5,88 @@ import java.util.List;
 
 import edu.kit.informatik.model.IP;
 
+/**
+ * Represents a Node of a Tree.
+ * 
+ * @author uiltc
+ * @version 1.0
+ */
 public class Node implements Comparable<Node> {
-    private IP value = null;
-    private List<Node> children = new ArrayList<>();
-    private Node parent = null;
+    private final IP value;
 
+    private Node parent = null;
+    private final List<Node> children = new ArrayList<>();
+
+    /**
+     * Creates a new instance of a Node.
+     * 
+     * @param value the IP that is stored in the Node
+     */
     public Node(IP value) {
         this.value = value;
     }
 
+    /**
+     * Adds children to this Node.
+     * 
+     * @param children list of Nodes that need to be added
+     */
     public void addChildren(List<Node> children) {
-        children.forEach(each -> each.setParent(this));
+        children.stream().forEach(each -> each.setParent(this));
         this.children.addAll(children);
     }
 
+    /**
+     * Adds one child to this Node.
+     * 
+     * @param child the child that needs to be added
+     * @return Node value of the added child
+     */
     public Node addChild(Node child) {
         child.setParent(this);
         this.children.add(child);
         return child;
     }
 
+    /**
+     * Gets the children of this Node.
+     * 
+     * @return list of Nodes of all the children
+     */
     public List<Node> getChildren() {
         return this.children;
     }
 
+    /**
+     * Gets the IP value of this Node.
+     * 
+     * @return the IP value of the Node
+     */
     public IP getValue() {
         return this.value;
     }
 
-    public void setValue(IP value) {
-        this.value = value;
-    }
-
+    /**
+     * Sets the parent of this Node.
+     * 
+     * @param parent the Node that needs to be set
+     */
     public void setParent(Node parent) {
         this.parent = parent;
     }
 
+    /**
+     * Gets the parent of this Node.
+     * 
+     * @return Node value of the parent
+     */
     public Node getParent() {
         return this.parent;
     }
 
+    // Compares this instance of Node with another Node
+    // It compares the IP values of both Nodes, thus we can use "compareTo"
+    // between two IPs
     @Override
     public int compareTo(Node o) {
         return this.value.compareTo(o.getValue());
