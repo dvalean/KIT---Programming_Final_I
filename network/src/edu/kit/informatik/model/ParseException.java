26736737aad1eb068b694532f78ef80477ef56d0
Daniel <danielvalean0@gmail.com>
2022-02-23 14:08:12 +0100

    comments for ParseException class

--- network/src/edu/kit/informatik/model/ParseException.java
+++ network/src/edu/kit/informatik/model/ParseException.java
@@ -1,6 +1,18 @@
 package edu.kit.informatik.model;
 
+/**
+ * Represents a ParseException.
+ * An extention of Exception for invalid parsing.
+ * 
+ * @author uiltc
+ * @version 1.0
+ */
 public class ParseException extends Exception {
+    /**
+     * Creates a new instance of ParseException.
+     * 
+     * @param message String value that explains the error
+     */
     public ParseException(String message) {
         super(message);
     }
