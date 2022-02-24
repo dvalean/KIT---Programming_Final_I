75ad8748e13354d52aaac8b7cc314075a719c901
Daniel <danielvalean0@gmail.com>
2022-02-24 16:02:19 +0100

    comments for ExceptionMessage class

--- network/src/edu/kit/informatik/resources/ExceptionMessage.java
+++ network/src/edu/kit/informatik/resources/ExceptionMessage.java
@@ -1,5 +1,11 @@
 package edu.kit.informatik.resources;
 
+/**
+ * Represents Exception Messages.
+ * 
+ * @author uiltc
+ * @version 1.0
+ */
 public enum ExceptionMessage {
     INVALID_IPV4("an invalid IP was entered."),
     INVALID_BRACKET_NOTATION("an invalid bracket notation for a network was entered.");
@@ -11,6 +17,7 @@ public enum ExceptionMessage {
         this.message = message;
     }
 
+    // Converts an ExceptionMassage to String
     @Override
     public String toString() {
         return PREFIX + this.message;
