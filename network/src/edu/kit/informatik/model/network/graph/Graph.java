a8da5ec69f5f12b1e92d742f5ee593a70c94f14f
Daniel <danielvalean0@gmail.com>
2022-02-24 16:21:16 +0100

    comments for Graph class

--- network/src/edu/kit/informatik/model/network/graph/Graph.java
+++ network/src/edu/kit/informatik/model/network/graph/Graph.java
@@ -55,10 +55,16 @@ public class Graph {
         }
     }
 
+    /**
+     * Gets the Edges of this Graph.
+     * 
+     * @return a list of Edges
+     */
     public List<Edge> getEdges() {
         return this.edges;
     }
 
+    // Creates an adjacency map for this Graph
     private void setAdjacency() {
         this.adjacency = new HashMap<>();
 
@@ -69,6 +75,7 @@ public class Graph {
         }
     }
 
+    // Makes two IPs adjacent and adds them to the adjacency map
     private void setAdjacent(IP source, IP destination) {
         source = contains(source);
         destination = contains(destination);
@@ -80,11 +87,18 @@ public class Graph {
         this.adjacency.get(source).add(destination);
     }
 
+    // Gets the adjacency map of this graph
     private Map<IP, List<IP>> getAdjacency() {
         setAdjacency();
         return this.adjacency;
     }
 
+    /**
+     * Determines if this graph containes an IP.
+     * 
+     * @param ip that is verified
+     * @return back the IP if it is contained and null otherwise
+     */
     public IP contains(IP ip) {
         for (Edge e : this.edges) {
             for (IP i : e.getNodes()) {
@@ -97,6 +111,14 @@ public class Graph {
         return null;
     }
 
+    /**
+     * Recursive method that determines if this graph has a loop or not.
+     * 
+     * @param ip      where the search begins
+     * @param root    where the IP originated
+     * @param visited what IPs were already checked
+     * @return true - if the graph has a loop and false - if the graph is loop-free
+     */
     public boolean isLooped(IP ip, IP root, List<IP> visited) {
         visited.add(ip);
         List<IP> adjacent = getAdjacency().get(ip);
@@ -118,6 +140,14 @@ public class Graph {
         return false;
     }
 
+    /**
+     * Calculates the height of graph (number of Edges in the longest path from the
+     * root).
+     * 
+     * @param root    where the calculation starts
+     * @param visited what IPs were already checked
+     * @return integer value of the height of the graph
+     */
     public int getHeight(IP root, List<IP> visited) {
         visited.add(root);
         List<Integer> height = new ArrayList<>();
@@ -132,6 +162,15 @@ public class Graph {
         return height.stream().mapToInt(each -> each).max().orElse(0);
     }
 
+    /**
+     * Gets the levels of this graph (where level 0 is the root).
+     * 
+     * @param root    where the calculation begins
+     * @param visited what IPs were already checked
+     * @param lvl     current level
+     * @param lvls    already known levels
+     * @return a list of lists* of IPs where every list* is a level
+     */
     public List<List<IP>> getLevels(IP root, List<IP> visited, int lvl, List<List<IP>> lvls) {
         if (!(lvl < lvls.size())) {
             lvls.add(new ArrayList<>());
@@ -151,6 +190,14 @@ public class Graph {
         return lvls;
     }
 
+    /**
+     * Removes an Edge between two IPs.
+     * 
+     * @param ip1 first IP that the Edge connects
+     * @param ip2 second IP that the Edge connects
+     * @return true - if the removal was successful and false - if the removal was
+     *         unsuccessful
+     */
     public boolean removeEdge(IP ip1, IP ip2) {
         for (Edge edge : this.edges) {
             if ((new HashSet<>(edge.getNodes())).equals(new HashSet<>(List.of(ip1, ip2)))) {
@@ -162,6 +209,15 @@ public class Graph {
         return false;
     }
 
+    /**
+     * A recursive method that determines the route from IP (start) to IP (end).
+     * 
+     * @param start   where the route begins
+     * @param end     where the route ends
+     * @param visited what IPs were already checked
+     * @param cpath   current path
+     * @return a list of IPs to get from start to end
+     */
     public List<IP> getRoute(IP start, IP end, List<IP> visited, List<IP> cpath) {
         cpath.add(start);
         visited.add(start);
@@ -186,6 +242,13 @@ public class Graph {
         return cpath;
     }
 
+    /**
+     * Converts a graph to BracketNotation String.
+     * 
+     * @param root    the root of the graph
+     * @param visited what IPs were already checked
+     * @return String value (BracketNotaion)
+     */
     public String toString(IP root, List<IP> visited) {
         visited.add(root);
         String result = root.toString();
